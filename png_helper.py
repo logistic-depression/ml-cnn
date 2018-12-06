822d4e2ab4de37231ee730462d8424fca0347917
Zyfarok <zyfarok@gmail.com>
2018-12-06 14:07:09 +0100

    Improve/fix png_helper

--- png_helper.py
+++ png_helper.py
@@ -3,11 +3,11 @@ import numpy as np
 import imageio
 
 def load_pngs(namepattern):
-    filelist = glob.glob(namepattern)
+    filelist = sorted(glob.glob(namepattern))
     return np.array([np.array(imageio.imread(fname)) for fname in filelist])
 
 def load_data():
-    train_x = load_pngs("training/images/satImage_???.png")
-    train_y = load_pngs("training/groundtruth/satImage_???.png")
-    test_x = load_pngs("test_set_images/test_*/test_*.png")
-    return (train_x, train_y), test_x
+    x_train = load_pngs("training/images/satImage_???.png")
+    y_train = load_pngs("training/groundtruth/satImage_???.png")
+    x_test = load_pngs("test_set_images/test_*/test_*.png")
+    return (x_train, y_train), x_test
