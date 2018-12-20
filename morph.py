37a9b3de3ef2063f46bb62a03cbc0fe9f711752c
sfdejong <sonny.dejong@epfl.ch>
2018-12-20 19:47:04 +0100

    bla

--- morph.py
+++ morph.py
@@ -2,6 +2,7 @@ import PIL, os
 from PIL import Image
 import cv2
 import numpy as np
+import skimage as skimage
 
 # BY RUNNING THIS FILE, ALL IMAGES IN filename ARE MORPHOLOGICALLY CLOSED
 # WITH A KERNEL SIZE OF 17x17, IF THEY HAVE THE RIGHT FILENAME (imageid)
@@ -10,7 +11,9 @@ def closeim(input_name, output_name, n):
     kernel = np.ones((n,n),np.uint8)
     img = np.asarray(Image.open(input_name))
     img = img[:,400:800,0]
-    img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+    img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+    seeds = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+    skimage.morphology.reconstruction(seeds, img, method='dilation')
     Image.fromarray(np.uint8(img)).save(output_name)
 
 filename = "predictions_training-v2/predictions_training/"
@@ -20,7 +23,7 @@ for i in range(1, 801):
         imageid = "prediction_%.d" % i
         imageid_out = "morphoutputImage_%.3d" % i
         input_name = filename + imageid + ".png"
-        output_name = filename + imageid_out + ".png"
+        output_name = filename + imageid_out + "b.png"
         closeim(input_name, output_name, 17)
     except:
 	    1+1
